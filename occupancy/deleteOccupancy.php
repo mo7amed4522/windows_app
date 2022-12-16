8cd1f29ef4e6ae14decb56e25c56e0f31bba4870
Kahled Mohamed <mo7amed4522@gmail.com>
2022-12-16 20:06:22 +0200

    new

--- occupancy/deleteOccupancy.php
+++ occupancy/deleteOccupancy.php
@@ -4,6 +4,6 @@ $ID =filterRequest('ID');
 $region = getOneData('nam_id','pathoccupancy',"`occ_id` = $ID");
 $place = getOneData('nam_pl','pathoccupancy',"`occ_id` = $ID");
 $occupancy = getOneData('name','occupancy',"`ID` = $ID");
-$path = "$region/$place,$occupancy";
+$path = "$region/$place/$occupancy";
 deleteFolder($path);
 deleteData("occupancy",$ID);
